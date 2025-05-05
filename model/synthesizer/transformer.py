ccfcb289fea555734bf79ecf9bd0e14f9bc43a18
Martin V. Larsen <martivl@stud.ntnu.no>
2025-05-05 20:46:51 +0200

    make GMM semi modual

--- model/synthesizer/transformer.py
+++ model/synthesizer/transformer.py
@@ -160,47 +160,52 @@ class DataTransformer():
         for id_, info in enumerate(self.meta):
             current = data[:, id_]
             if info['type'] == "continuous":
-                if info["name"] not in self.general_columns: 
-                  current = current.reshape([-1, 1])
-                  means = self.model[id_].means_.reshape((1, self.n_clusters))
-                  stds = np.sqrt(self.model[id_].covariances_).reshape((1, self.n_clusters))
-                  features = np.empty(shape=(len(current),self.n_clusters))
-                  if ispositive == True:
-                      if id_ in positive_list:
-                          features = np.abs(current - means) / (4 * stds)
-                  else:
-                      features = (current - means) / (4 * stds)
+                if info["name"] not in self.general_columns:
+                    transformer = info['transformer'] 
+                    features, re_ordered_phot = transformer.transform(current)
+                    """
+                    current = current.reshape([-1, 1])
+                    means = self.model[id_].means_.reshape((1, self.n_clusters))
+                    stds = np.sqrt(self.model[id_].covariances_).reshape((1, self.n_clusters))
+                    features = np.empty(shape=(len(current),self.n_clusters))
+                    if ispositive == True:
+                        if id_ in positive_list:
+                            features = np.abs(current - means) / (4 * stds)
+                    else:
+                        features = (current - means) / (4 * stds)
 
-                  probs = self.model[id_].predict_proba(current.reshape([-1, 1]))
-                  n_opts = sum(self.components[id_])
-                  features = features[:, self.components[id_]]
-                  probs = probs[:, self.components[id_]]
+                    probs = self.model[id_].predict_proba(current.reshape([-1, 1]))
+                    n_opts = sum(self.components[id_])
+                    features = features[:, self.components[id_]]
+                    probs = probs[:, self.components[id_]]
 
-                  opt_sel = np.zeros(len(data), dtype='int')
-                  for i in range(len(data)):
-                      pp = probs[i] + 1e-6
-                      pp = pp / sum(pp)
-                      opt_sel[i] = np.random.choice(np.arange(n_opts), p=pp)
+                    opt_sel = np.zeros(len(data), dtype='int')
+                    for i in range(len(data)):
+                        pp = probs[i] + 1e-6
+                        pp = pp / sum(pp)
+                        opt_sel[i] = np.random.choice(np.arange(n_opts), p=pp)
 
-                  idx = np.arange((len(features)))
-                  features = features[idx, opt_sel].reshape([-1, 1])
-                  features = np.clip(features, -.99, .99) 
-                  probs_onehot = np.zeros_like(probs)
-                  probs_onehot[np.arange(len(probs)), opt_sel] = 1
+                    idx = np.arange((len(features)))
+                    features = features[idx, opt_sel].reshape([-1, 1])
+                    features = np.clip(features, -.99, .99) 
+                    probs_onehot = np.zeros_like(probs)
+                    probs_onehot[np.arange(len(probs)), opt_sel] = 1
 
-                  re_ordered_phot = np.zeros_like(probs_onehot)
-                  
-                  col_sums = probs_onehot.sum(axis=0)
-                  
+                    re_ordered_phot = np.zeros_like(probs_onehot)
+                    
+                    col_sums = probs_onehot.sum(axis=0)
+                    
 
-                  n = probs_onehot.shape[1]
-                  largest_indices = np.argsort(-1*col_sums)[:n]
-                  self.ordering.append(largest_indices)
-                  for id,val in enumerate(largest_indices):
-                      re_ordered_phot[:,id] = probs_onehot[:,val]
-                
-                  
-                  values += [features, re_ordered_phot]
+                    n = probs_onehot.shape[1]
+                    largest_indices = np.argsort(-1*col_sums)[:n]
+                    self.ordering.append(largest_indices)
+                    for id,val in enumerate(largest_indices):
+                        re_ordered_phot[:,id] = probs_onehot[:,val]
+                    """
+                    self.ordering.append(None) # append to keep the same length as the meta list
+                    values += [features, re_ordered_phot]
+
+                    
                   
                 else:
                   
@@ -317,34 +322,38 @@ class DataTransformer():
         for id_, info in enumerate(self.meta):
             if info['type'] == "continuous":
                 if info["name"] not in self.general_columns:
-                  u = data[:, st]
-                  v = data[:, st + 1:st + 1 + np.sum(self.components[id_])]
-                  order = self.ordering[id_] 
-                  v_re_ordered = np.zeros_like(v)
+                    transformer = info['transformer']
+                    tmp, inv_ids = transformer.inverse_transform(data,st)
+                    """
+                    u = data[:, st]
+                    v = data[:, st + 1:st + 1 + np.sum(self.components[id_])]
+                    order = self.ordering[id_] 
+                    v_re_ordered = np.zeros_like(v)
 
-                  for id,val in enumerate(order):
-                      v_re_ordered[:,val] = v[:,id]
-                  
-                  v = v_re_ordered
+                    for id,val in enumerate(order):
+                        v_re_ordered[:,val] = v[:,id]
+                    
+                    v = v_re_ordered
 
-                  u = np.clip(u, -1, 1)
-                  v_t = np.ones((data.shape[0], self.n_clusters)) * -100
-                  v_t[:, self.components[id_]] = v
-                  v = v_t
-                  st += 1 + np.sum(self.components[id_])
-                  means = self.model[id_].means_.reshape([-1])
-                  stds = np.sqrt(self.model[id_].covariances_).reshape([-1])
-                  p_argmax = np.argmax(v, axis=1)
-                  std_t = stds[p_argmax]
-                  mean_t = means[p_argmax]
-                  tmp = u * 4 * std_t + mean_t
+                    u = np.clip(u, -1, 1)
+                    v_t = np.ones((data.shape[0], self.n_clusters)) * -100
+                    v_t[:, self.components[id_]] = v
+                    v = v_t
+                    st += 1 + np.sum(self.components[id_])
+                    means = self.model[id_].means_.reshape([-1])
+                    stds = np.sqrt(self.model[id_].covariances_).reshape([-1])
+                    p_argmax = np.argmax(v, axis=1)
+                    std_t = stds[p_argmax]
+                    mean_t = means[p_argmax]
+                    tmp = u * 4 * std_t + mean_t
                                     
-                  for idx,val in enumerate(tmp):
-                     if (val < info["min"]) | (val > info['max']):
-                         invalid_ids.append(idx)
-                  
+                    for idx,val in enumerate(tmp):
+                        if (val < info["min"]) | (val > info['max']):
+                            invalid_ids.append(idx)
+                    """
                 
-                  data_t[:, id_] = tmp
+                    data_t[:, id_] = tmp
+                    invalid_ids.extend(inv_ids)
 
                 else:
                   u = data[:, st]
